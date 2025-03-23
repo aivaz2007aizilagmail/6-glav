8f3b0880bcca8f4d6cee50faacee02cda7200225
Aivaz <105557584+aivaz2007aizilagmail@users.noreply.github.com>
2025-03-23 11:14:53 +0300

    Update stepen.c
    Fix issue #3: разделить логические блоки кода пустой строчкой
    сделать декомпозицию алгоритма (разделить на несколько строчек - простых действий, так, чтобы было понятно, что выполняет каждое атомарное действие)
    сделать анализ алгоритма и добавить комментарии (с примерами) принципа работы алгоритма
    объяснить использование бинарного оператора (этот вопрос будет и в теории) И / ИЛИ

--- stepen.c
+++ stepen.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
 int main() {
+
     int n;
+    
     printf("Введите целое число: ");
     scanf("%d", &n);
-    int is_power_of_two = (n > 0) && ((n & (n - 1)) == 0);
-    printf("%d\n", is_power_of_two);
+
+    // Проверка что число положительное
+    int is_positive = (n > 0);          
+    
+    // Проверка битового представления
+    int is_power_of_two = (n & (n - 1)) == 0; 
+    
+    // Общий результат
+    int result = is_positive && is_power_of_two; 
+
+    // Вывод результата (1 - степень двойки, 0 - нет)
+    printf("%d\n", result);
+
     return 0;
 }
